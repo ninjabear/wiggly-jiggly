5ef9284c4b38d254a9eadec5f9d0f6a38f3e4ec7
Ed Lewis <ninjabear@users.noreply.github.com>
2023-06-16 10:46:10 +0100

    Add jiggler

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module ninjabear/wiggly-jiggly
 
 go 1.13
+
+require github.com/go-vgo/robotgo v0.100.10
--- wiggly-jiggly.go
+++ wiggly-jiggly.go
@@ -1,7 +1,37 @@
 package main
 
 import "fmt"
+import "bufio"
+import "os"
+import "time"
+import "github.com/go-vgo/robotgo"
+
+func jiggly() {
+	time.Sleep(1 * time.Second)
+	robotgo.MoveRelative(0, -10)
+	robotgo.MoveRelative(-10, 0)
+	robotgo.MoveRelative(10, 0)
+	robotgo.MoveRelative(0, 10)
+}
+
+func doMouseStuff(tx chan bool) {
+	for {
+		select {
+		case <-tx:
+			return
+		default:
+			jiggly()
+		}
+	}
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	ch := make(chan bool)
+	go doMouseStuff(ch)
+	fmt.Println("========== wiggly-jiggyly ==============")
+	fmt.Println("======== press enter to exit ===========")
+	reader := bufio.NewReader(os.Stdin)
+	reader.ReadString('\n')
+	ch <- true
+	fmt.Println("=============== bye! ===================")
 }
